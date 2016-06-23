3382ca757886ffe4b4ffea5b4d565a738a793b2c
Haipeng <njucs_wuhaipeng@163.com>
2016-06-23 11:08:03 +0800

    support config file

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "encoding/json"
     "database/sql"
     "fmt"
+    "io/ioutil"
     "log"
     "net/http"
     "os"
@@ -35,6 +37,14 @@ func connect(host, port, user, password, database string) error {
     return db.Ping()
 }
 
+type Config struct {
+    Address  string `json:"mysql_address"`
+    Port     string `json:"mysql_port"`
+    User     string `json:"mysql_user"`
+    Password string `json:"mysql_password"`
+    DB       string `json:"mysql_db"`
+}
+
 func main() {
     address := os.Getenv("MYSQL_ADDRESS")
     port := os.Getenv("MYSQL_PORT")
@@ -42,8 +52,19 @@ func main() {
     passwd := os.Getenv("MYSQL_PASSWORD")
     db := os.Getenv("MYSQL_DATABASE")
 
+    if bytes, err := ioutil.ReadFile("/tmp/web.json"); err == nil {
+        conf := new(Config)
+        if json.Unmarshal(bytes, conf) == nil {
+            address = conf.Address
+            port = conf.Port
+            user = conf.User
+            passwd = conf.Password
+            db = conf.DB
+        }
+    }
+
     if len(address) == 0 || len(port) == 0 || len(user) == 0 || len(passwd) == 0 || len(db) == 0 {
-        log.Fatalln("Invalid mysql env")
+        log.Fatalln("Invalid mysql connection args")
     }
 
     if err := connect(address, port, user, passwd, db); err != nil {
@@ -53,5 +74,5 @@ func main() {
     http.HandleFunc("/hello", hello)
     http.HandleFunc("/env", env)
 
-    log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
